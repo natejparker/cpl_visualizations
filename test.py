71d149e3c52cc4461e78b53e70d0a710303c937a
rahuldmello24 <44767369+rahuldmello24@users.noreply.github.com>
2023-12-02 16:55:05 -0500

    Add new files

--- test.py
+++ test.py
@@ -10,9 +10,17 @@ plot_data = pd.DataFrame({
     'Year': chicago_df['Year'][:-2]
 })
 
+average_visitors_per_month = plot_data.groupby('Month')['Total Visitors'].mean().reset_index()
+
+# Create the initial line plot
 fig = px.line(plot_data, x='Month', y='Total Visitors', color='Year',
               title='Total Number of Visitors by Month in Chicago Libraries',
               labels={'Total Visitors': 'Number of Visitors'},
               category_orders={'Month': ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]})
 
+# Add the average line
+fig.add_scatter(x=average_visitors_per_month['Month'], y=average_visitors_per_month['Total Visitors'],
+                mode='lines', name='Average', line=dict(color='black', dash='dash'))
+
+# Show the plot
 fig.show()
